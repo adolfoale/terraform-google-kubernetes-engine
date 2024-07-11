4ec079467f81e8ae2172e1f7755578e0cefe574b
adolfoale <adolfoalejandroacevedo@gmail.com>
2024-07-11 11:14:31 -0400

    chg:  region

--- examples/safer_cluster_iap_bastion/variables.tf
+++ examples/safer_cluster_iap_bastion/variables.tf
@@ -28,7 +28,7 @@ variable "cluster_name" {
 variable "region" {
   type        = string
   description = "The region to host the cluster in"
-  default     = "us-east1"
+  default     = "us-central1"
 
 }
 
